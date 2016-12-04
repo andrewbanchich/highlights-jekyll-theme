32cf521f4d7c0da670c8bc7ff6d58c57ccef96d7
Andrew Banchich <andrewbanchich@gmail.com>
2016-12-03 22:50:30 -0500

    paths

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,7 +170,7 @@
 
 						$this
 							.css('background-image', 'url("' + $primaryImg.attr('src') + '")')
-							.css('-ms-behavior', 'url("css/ie/backgroundsize.min.htc")');
+							.css('-ms-behavior', 'url("assets/css/ie/backgroundsize.min.htc")');
 
 						return;
 
@@ -179,7 +179,7 @@
 				// Create bg and append it to body.
 					$bg = $('<div class="main-bg" id="' + $this.attr('id') + '-bg"></div>')
 						.css('background-image', (
-							'url("css/images/overlay.png"), url("' + $primaryImg.attr('src') + '")'
+							'url("assets/css/images/overlay.png"), url("' + $primaryImg.attr('src') + '")'
 						))
 						.appendTo($body);
 
